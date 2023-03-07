b0da396dad12777a508fded65719db9f2a06ca5e
Muhammed Cosan <muha0653@edu.easj.dk>
2023-03-07 09:33:20 +0100

    Add project files.

---
+++ Opgave1/Car.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opgave1
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Model { get; set; }
+        public double Price { get; set; }
+        public string LicensePlate { get; set; }
+
+        public void ValidateId()
+        {
+            if (Id == 0)
+            {
+                throw new ArgumentNullException("Id is 1 letter");
+            }
+
+        }
+        public void ValidateModel()
+        {
+            if (Model == null)
+            {
+                throw new ArgumentNullException();
+            }
+            if (Model.Length <= 4)
+            {
+                throw new ArgumentOutOfRangeException(" Længden er minimum 4");
+            }
+        }
+        public void ValidatePrice()
+        {
+            if (Price <= 0)
+            {
+                throw new ArgumentOutOfRangeException("Prisen Skal være Positiv");
+            }
+        }
+        public void ValidateLicensePlate()
+        {
+            if (LicensePlate == null)
+            {
+                throw new ArgumentNullException();
+            }
+            if (LicensePlate.Length < 1 && LicensePlate.Length > 8)
+            {
+                throw new ArgumentOutOfRangeException("Talet skal være mellem 2 og 7");
+            }
+
+        }
+        public void Validate()
+        {
+            ValidateId();
+            ValidateModel();
+            ValidatePrice();
+            ValidateLicensePlate();
+
+
+        }
+    }
+}
---
+++ Opgave1Tests/CarTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Opgave1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opgave1.Tests
+{
+    [TestClass()]
+    public class CarTests
+    { 
+        public Car car = new Car() { Id = 1, LicensePlate = "ZX36434", Model = "Mazda 3", Price = 1 };
+        public Car neCar = new Car() { Id = 0, LicensePlate = "Mo2635", Model = "Omg 2", Price = 1 };
+        public Car ModelMindreend4 = new Car() { Id = 2, LicensePlate = "CZ75-Auto", Model = "123", Price = 125000 };
+        public Car ModelNullCar = new Car() { Id = 3, LicensePlate = "GingGung123", Model = null, Price = 240 };
+        public Car NegCarPrice = new Car() { Id = 1, LicensePlate = "AlbSv241", Model = "I-5F", Price =-4};
+
+
+        [TestMethod()]
+        public void ValidateIdTest()
+        {
+            car.ValidateId();
+            Assert.ThrowsException<ArgumentNullException>(() => neCar.ValidateId());
+        }
+
+        [TestMethod()]
+        public void ValidateModelTest()
+        {
+            car.ValidateModel();
+            Assert.ThrowsException<ArgumentNullException>(() => ModelNullCar.ValidateModel());
+            Assert.ThrowsException<ArgumentOutOfRangeException>(ModelMindreend4.ValidateModel);
+        }
+        [TestMethod()]
+        public void ValidatePriceTest()
+        {
+            car.ValidatePrice();
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => NegCarPrice.ValidatePrice());
+        }
+    }
+}
